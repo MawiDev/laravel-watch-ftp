b82b71e3306c3c79cf6fd73c2f1237962dec9710
RisaDev <mawidev@proton.me>
2023-12-03 15:17:49 +0100

    Update 1.0.3

--- setup-laravel-watch-ftp.js
+++ setup-laravel-watch-ftp.js
@@ -1,20 +1,58 @@
-const fs = require('fs');
+const fs = require('fs').promises; // Utilizzo di fs.promises per gestire le promesse
 const path = require('path');
 
-// Directory e file da creare
-const watcherFtpDir = 'watcher-ftp';
-const configFilePath = path.join(watcherFtpDir, 'config.json');
+async function createConfigDirectory() {
+  const watcherFtpDir = path.join(__dirname, '..', '..', 'watcher-ftp');
 
-// Contenuto del file di configurazione
-const configContent = JSON.stringify({
-  // Inserisci qui le tue opzioni di configurazione predefinite
-}, null, 2);
-
-// Creazione della directory e del file
-try {
-  fs.mkdirSync(watcherFtpDir);
-  fs.writeFileSync(configFilePath, configContent);
-  console.log('Laravel Watch FTP configurato con successo!');
-} catch (err) {
-  console.error('Errore durante la configurazione di Laravel Watch FTP:', err);
+  try {
+    await fs.mkdir(watcherFtpDir);
+    console.log('Directory "watcher-ftp" creata con successo.');
+  } catch (error) {
+    if (error.code === 'EEXIST') {
+      console.log('La directory "watcher-ftp" esiste già.');
+    } else {
+      console.error('Errore durante la creazione della directory "watcher-ftp":', error);
+    }
+  }
 }
+
+async function createConfigFile() {
+  const watcherFtpDir = path.join(__dirname, '..', '..', 'watcher-ftp');
+  const configFilePath = path.join(watcherFtpDir, 'config.json');
+
+  const configContent = JSON.stringify({}, null, 2);
+
+  try {
+    await fs.writeFile(configFilePath, configContent);
+    console.log('File di configurazione creato con successo.');
+  } catch (error) {
+    console.error('Errore durante la creazione del file di configurazione:', error);
+  }
+}
+
+async function addWatchScriptToPackageJson() {
+  const projectPackageJsonPath = path.join(process.cwd(), 'package.json');
+  const watchScript = '"watch": "node ./node_modules/laravel-watch-ftp/main.js"';
+
+  try {
+    const projectPackageJson = require(projectPackageJsonPath);
+    projectPackageJson.scripts = projectPackageJson.scripts || {};
+
+    if (!projectPackageJson.scripts.watch) {
+      projectPackageJson.scripts.watch = watchScript;
+      await fs.writeFile(projectPackageJsonPath, JSON.stringify(projectPackageJson, null, 2));
+      console.log('Script "watch" aggiunto al package.json con successo!');
+    } else {
+      console.log('Lo script "watch" è già presente nel package.json.');
+    }
+  } catch (error) {
+    console.error('Errore durante l\'aggiunta dello script "watch" al package.json:', error);
+  }
+}
+
+// Esegui le funzioni
+(async () => {
+  await createConfigDirectory();
+  await createConfigFile();
+  await addWatchScriptToPackageJson();
+})();
